736b076aa7671463dca4cd16c1b43f45382777f3
gostekk <gostekk94@gmail.com>
2018-01-21 00:35:10 +0100

    Refactor #1

--- main.cpp
+++ main.cpp
@@ -11,63 +11,63 @@ void SaveImage(QImage naszobraz, QString DirectionToSaveImage) {
 }
 
 void ConvertFromFishEye(
-	QImage naszobraz, 
+	QImage naszobraz,
 	QString ConversionType,
 	double vStrenght,
-	double vRadius,
+	double vFoc,
 	QString DirectionToSaveImage) {
 
-  int W,H,WO,HO,xd,yd;
-  // float foc=4.0/7.3,
-  float foc=4.0/12.0;
-  float r,xf,yf,cosfi,sinfi,D,DO;
+  int vImageWidth,vImageHeight,vHalfImageWidth,vHalfImageHeight,xd,yd;
+  // float vFoc=4.0/7.3,
+  // float vFoc=4.0/12.0;
+  float vRadius,xf,yf,cosfi,sinfi,vDistanceSource,vDistanceNew;
   //float vStrenght=0.78;
 
-  W = naszobraz.width();
-  H = naszobraz.height();
-  WO = naszobraz.width()/2;
-  HO = naszobraz.height()/2;
+  vImageWidth = naszobraz.width();
+  vImageHeight = naszobraz.height();
+  vHalfImageWidth = naszobraz.width()/2;
+  vHalfImageHeight = naszobraz.height()/2;
 
-  QImage convertedImage(WO,HO, QImage::Format_RGB32);
+  QImage convertedImage(vHalfImageWidth,vHalfImageHeight, QImage::Format_RGB32);
 
-  DO=foc*qSqrt(WO*WO+HO*HO);
-  D=foc*qSqrt(W*W+H*H);
+  vDistanceNew = vFoc *qSqrt(vHalfImageWidth*vHalfImageWidth+vHalfImageHeight*vHalfImageHeight);
+  vDistanceSource = vFoc *qSqrt(vImageWidth*vImageWidth+vImageHeight*vImageHeight);
 
-  for(int ix = 0; ix < WO; ix++) {
-    for (int iy=0; iy < HO; iy++) {
-		
-      xf = (float)(ix-WO/2);
-      yf = (float)(iy-HO/2);
-      r = qSqrt( xf*xf+yf*yf ) * vStrenght;
-      cosfi = xf / r;
-      sinfi = yf / r;
- 
+  for(int ix = 0; ix < vHalfImageWidth; ix++) {
+    for (int iy=0; iy < vHalfImageHeight; iy++) {
+
+      xf = (float)(ix-vHalfImageWidth/2);
+      yf = (float)(iy-vHalfImageHeight/2);
+      vRadius = qSqrt( xf*xf+yf*yf ) * vStrenght;
+      cosfi = xf / vRadius;
+      sinfi = yf / vRadius;
+
 	if (ConversionType == "Stereographic") {
 		// Stereographic // vStrenght = 0.78
-		xf = cosfi * 2 * D * qTan(0.5 * qAtan2(r,DO));
-		yf = sinfi * 2 * D * qTan(0.5 * qAtan2(r,DO));
+		xf = cosfi * 2 * vDistanceSource * qTan(0.5 * qAtan2(vRadius,vDistanceNew));
+		yf = sinfi * 2 * vDistanceSource * qTan(0.5 * qAtan2(vRadius,vDistanceNew));
 	}
 	if (ConversionType == "Equidistant") {
 		// Equidistant // vStrenght =
-		xf = cosfi * D * qAtan2(r,DO);
-		yf = sinfi * D * qAtan2(r,DO);
+		xf = cosfi * vDistanceSource * qAtan2(vRadius,vDistanceNew);
+		yf = sinfi * vDistanceSource * qAtan2(vRadius,vDistanceNew);
 	}
 	if (ConversionType == "Equisolid") {
 		// Equisolid angle // vStrenght =
-		xf = cosfi * 2 * D * qSin(0.5 * qAtan2(r,DO));
-		yf = sinfi * 2 * D * qSin(0.5 * qAtan2(r,DO));
+		xf = cosfi * 2 * vDistanceSource * qSin(0.5 * qAtan2(vRadius,vDistanceNew));
+		yf = sinfi * 2 * vDistanceSource * qSin(0.5 * qAtan2(vRadius,vDistanceNew));
 	}
 	if (ConversionType == "Orthographic") {
 		// Orthographic // vStrenght = 1.2
-		xf = cosfi * D * qSin(qAtan2(r,DO));
-		yf = sinfi * D * qSin(qAtan2(r,DO));
+		xf = cosfi * vDistanceSource * qSin(qAtan2(vRadius,vDistanceNew));
+		yf = sinfi * vDistanceSource * qSin(qAtan2(vRadius,vDistanceNew));
 	}
       xd = (int)xf;
       yd = (int)yf;
-      xd += W / 2;
-      yd += H / 2;
+      xd += vImageWidth / 2;
+      yd += vImageHeight / 2;
 
-      if ((xd>0)&&(xd<W)&&(yd>0)&&(yd<H)) {
+      if ((xd>0)&&(xd<vImageWidth)&&(yd>0)&&(yd<vImageHeight)) {
         QColor NewColorOfPixel = naszobraz.pixel(xd, yd);
         convertedImage.setPixel(ix, iy, NewColorOfPixel.rgba());
       }
